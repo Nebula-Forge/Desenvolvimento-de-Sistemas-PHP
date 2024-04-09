6bda302b06ccdb2622e4b2e21f645d321c89455c
Daniel Tavares <133452829+DannyahIA@users.noreply.github.com>
2024-04-09 11:24:27 -0300

    Add files via upload

---
+++ aulaphp/AULA07/formulario.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+</head>
+<body>
+
+    <h1> Formulário </h1>
+
+    <form action="resposta.php" method="get">
+
+        <label for="">Nome: </label>
+        <input type="text" name="nomePessoa" id="">
+
+        <label for="">Sobrenome: </label>
+        <input type="text" name="sobrenomePessoa" id="">
+
+        <input type="submit" value="Enviar">
+
+    </form>
+    
+    <!-- <form action="" method="get"></form>
+    <form action="" method="post"></form> -->
+    
+</body>
+</html>
